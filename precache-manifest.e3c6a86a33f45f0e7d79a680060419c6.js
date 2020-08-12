178d46d35f3bd4aea15375c3475375797a7ac8de
Delma <ephyadelma17@gmail.com>
2020-08-12 04:52:49 +0000

    Updates

---
+++ precache-manifest.e3c6a86a33f45f0e7d79a680060419c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783f62d427e71640157c527adf155a07",
+    "url": "/reactGtl1/index.html"
+  },
+  {
+    "revision": "8e0ef45713d4b23f9b5d",
+    "url": "/reactGtl1/static/css/main.feff7999.chunk.css"
+  },
+  {
+    "revision": "71229c1c2a04f77a7f38",
+    "url": "/reactGtl1/static/js/2.4b0bffa4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactGtl1/static/js/2.4b0bffa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e0ef45713d4b23f9b5d",
+    "url": "/reactGtl1/static/js/main.ac41216c.chunk.js"
+  },
+  {
+    "revision": "78b5c1a31eff534adeef",
+    "url": "/reactGtl1/static/js/runtime-main.621b7a52.js"
+  },
+  {
+    "revision": "3efa2cb8905183c81360982460d99397",
+    "url": "/reactGtl1/static/media/IMG_20181105_143342_0_1541428674806.3efa2cb8.jpg"
+  }
+]);
